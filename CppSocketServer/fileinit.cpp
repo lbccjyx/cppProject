da4095d25856fe802944b3786e0a50fcc9c8d36b
lbccjyx <1033070036@qq.com>
2024-02-27 17:26:53 +0800

    一个CPP的socket服务端 功能是监听某端口转为cmd命令。优势是， 1：另开线程打印标准输出， 2：不使用system，无需等待命令结束，可以立刻释放监听的端口

---
+++ CppSocketServer/fileinit.cpp
@@ -0,0 +1,27 @@
+#include "fileinit.h"
+
+
+threadPrint* threadPrint::instance = nullptr;
+
+
+void init()
+{
+    system("CHCP 65001");
+    if (__cplusplus == 202101L)
+        std::cout << "C++23"<<std::endl;
+    else if (__cplusplus == 202002L)
+        std::cout << "C++20"<<std::endl;
+    else if (__cplusplus == 201703L)
+        std::cout << "C++17"<<std::endl;
+    else if (__cplusplus == 201402L)
+        std::cout << "C++14"<<std::endl;
+    else if (__cplusplus == 201103L)
+        std::cout << "C++11"<<std::endl;
+    else if (__cplusplus == 199711L)
+        std::cout << "C++98"<<std::endl;
+    else
+        std::cout << "pre-standard C++." << __cplusplus; 
+
+    std::cout << "pre-standard C++." << __cplusplus<<std::endl;
+    std::cout << "\n";
+}
---
+++ CppSocketServer/listen_cmd_server.cpp
@@ -0,0 +1,114 @@
+#include "fileinit.h"
+
+using namespace std;
+// 删除字符串后面的换行符
+static inline void removeTrailingNewline(std::string &s) {
+    s.erase(s.find_last_not_of("\n\r") + 1);
+}
+
+bool executeCommand(const wchar_t* command, const wchar_t* workingDirectory) {
+	// 初始化STARTUPINFO和PROCESS_INFORMATION结构
+	STARTUPINFO si;
+	PROCESS_INFORMATION pi;
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(si);
+	ZeroMemory(&pi, sizeof(pi));
+    si.hStdOutput = NULL;
+
+	wchar_t* commandCopy = _wcsdup(command);
+
+	// 创建进程
+	if (CreateProcess(NULL, commandCopy, NULL, NULL, FALSE, 0, NULL,
+		workingDirectory, &si, &pi)) {
+		// 等待进程结束
+		WaitForSingleObject(pi.hProcess, INFINITE);
+
+		// 关闭进程和线程的句柄
+		CloseHandle(pi.hProcess);
+		CloseHandle(pi.hThread);
+
+        XXPrint("Command executed successfully.\n");
+		return true;
+	}
+	else {
+        XXPrint("Failed to execute command.\n");
+		return false;
+	}
+}
+
+unsigned __stdcall Answer(void* a) {
+  Socket* s = (Socket*) a;
+
+  while (1) {
+    std::string r = s->ReceiveLine();
+    if (r.empty()) break;
+    removeTrailingNewline(r);
+    XXPrint("ReceiveLine:", r, "\n");
+
+    if (myCmdDataJson.contains(r))
+    {
+		std::string strDir = myCmdDataJson[r]["dir"];
+		std::string strCMD = myCmdDataJson[r]["cmd"];
+        if(strDir.length() <= 0 || strCMD.length() <= 0)
+            break;
+
+        XXPrint("匹配命令:\tDIR:", strDir,"\tCMD:", strCMD, "\n");
+        wchar_t* wcDir = new wchar_t[strDir.size()];
+        swprintf(wcDir, strDir.size() + 1, L"%S", strDir.c_str());
+
+		wchar_t* wcCMD = new wchar_t[strCMD.size()];
+		swprintf(wcCMD, strCMD.size() + 1, L"%S", strCMD.c_str());
+
+        executeCommand(wcCMD, wcDir);
+    }
+  }
+
+  s->Close();
+  delete s;
+
+  return 0;
+}
+
+void threadFunction() {
+  std::ifstream f("listen_cmd_server_cfg.json");
+  if (!f) {
+      XXPrint("无法打开文件 cmd.json\n");
+      return ;
+  }
+  try {
+    myCmdDataJson = json::parse(f);
+  } catch (json::parse_error& e) {
+      XXPrint("解析JSON数据时发生错误: " ,e.what(),'\n');
+      return ;
+  }
+  
+  XXPrint("listen ", myCmdDataJson["listenPort"]," for restart_server\n");
+  try {
+    SocketServer in(myCmdDataJson["listenPort"], 30);
+    
+    while (1) {
+      Socket* s=in.Accept();
+
+      unsigned ret;
+      _beginthreadex(0,0,Answer,(void*) s,0,&ret);
+    }
+  } catch (const char* msg) {
+      XXPrint("初始化失败 端口占用", msg, "\n 所以需要换个端口 \n");
+  }
+}
+
+int main(int argc, char* argv[]) {
+  init();
+
+  threadPrint* a = new threadPrint();
+  std::thread tPrint([a] { a->worker(); });
+
+  std::thread t(threadFunction);
+  t.join();
+
+  a->over();
+  tPrint.join();
+
+
+  return 0;
+}
