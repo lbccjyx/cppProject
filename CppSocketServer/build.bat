290188ed9bbc8f74ab3f462672164c0254598261
lbccjyx <1033070036@qq.com>
2024-03-11 20:15:20 +0800

    自动编译生成

--- CppSocketServer/build.bat
+++ CppSocketServer/build.bat
@@ -1,8 +1,12 @@
-@ECHO OFF
+@ECHO ON
 
-IF NOT EXIST build MKDIR build
-:: build
-PUSHD build	 	
-conan install ..
-conan build ..
+set BASEDIR=%~dp0
+PUSHD %BASEDIR%
 
+RMDIR /Q /S build
+
+conan install . --output-folder=build --build=missing
+cd build
+cmake .. -G "Visual Studio 17 2022" -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake
+cmake --build . --config Release
+Release\compressor.exe
