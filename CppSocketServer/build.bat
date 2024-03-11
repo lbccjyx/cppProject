4c677aaba78be16ba12dda09935bbb2b650526d1
lbccjyx <1033070036@qq.com>
2024-03-11 20:22:39 +0800

    fix::build.bat

--- CppSocketServer/build.bat
+++ CppSocketServer/build.bat
@@ -7,6 +7,7 @@ RMDIR /Q /S build
 
 conan install . --output-folder=build --build=missing
 cd build
-cmake .. -G "Visual Studio 17 2022" -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake
+cmake .. -G "Visual Studio 17 2022" -DCMAKE_TOOLCHAIN_FILE=.\conan_toolchain.cmake -DCMAKE_POLICY_DEFAULT_CMP0091=NEW
 cmake --build . --config Release
-Release\compressor.exe
+cmake --build . --config Debug
+@REM Release\xxx.exe
