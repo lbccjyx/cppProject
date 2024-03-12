33eca51fd78ace88ce6f8d211a46d685110be70b
lbccjyx <1033070036@qq.com>
2024-03-12 11:46:52 +0800

    使用虚拟环境

--- CppSocketServer/build.bat
+++ CppSocketServer/build.bat
@@ -4,10 +4,20 @@ set BASEDIR=%~dp0
 PUSHD %BASEDIR%
 
 RMDIR /Q /S build
+@REM 创建虚拟环境
+python -m venv .venv
+@REM 使用虚拟环境
+call .\.venv\Scripts\activate
 
+python.exe -m pip install --upgrade pip
+@REM 虚拟环境安装conan
+pip install conan
 conan install . --output-folder=build --build=missing
 cd build
 cmake .. -G "Visual Studio 17 2022" -DCMAKE_TOOLCHAIN_FILE=.\conan_toolchain.cmake -DCMAKE_POLICY_DEFAULT_CMP0091=NEW
 cmake --build . --config Release
 cmake --build . --config Debug
 @REM Release\xxx.exe
+
+@REM 退出虚拟环境
+deactivate
