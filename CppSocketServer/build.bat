697b240ec24363cc24d37e9ee72df6526dade0f3
lbccjyx <1033070036@qq.com>
2024-03-20 14:49:29 +0800

    应该是还没有链接到lib

--- CppSocketServer/build.bat
+++ CppSocketServer/build.bat
@@ -20,9 +20,12 @@ cd build
 @REM 																	  CMAKE_PREFIX_PATH是要编译的文件所在的安装路径
 cmake .. -G "Visual Studio 17 2022"  -DCMAKE_POLICY_DEFAULT_CMP0091=NEW -DCMAKE_PREFIX_PATH=build 
 
-cmake --build . --config Release
-cmake --build . --config Debug
+cmake --build . --config Release -- /m
+cmake --build . --config Debug -- /m
+
 @REM Release\xxx.exe
+@REM start devenv build\CppSocketServer.sln
+
 
 @REM 退出虚拟环境
 deactivate
