2dde62333924ba316369a20b32c6c64825f465e5
lbccjyx <1033070036@qq.com>
2024-03-25 20:16:44 +0800

    整理代码

--- CppSocketServer/test/unit_test2.cpp
+++ CppSocketServer/test/unit_test2.cpp
@@ -11,6 +11,19 @@
 
 BOOST_AUTO_TEST_SUITE(tests_suit)
 
+struct AmuletCreateReq
+{
+	INT64 arrayIdCostItem[0];
+};
+void PrintArray0(AmuletCreateReq& Arc)
+{
+
+	for (int i = 0; i < 5; ++i)
+	{
+		std::cout << Arc.arrayIdCostItem[i] << std::endl;
+	}
+}
+
 BOOST_AUTO_TEST_CASE(my_test6)
 {
 	std::cout << "\n\n test6 boost::scoped_ptr\n";
@@ -29,13 +42,9 @@ BOOST_AUTO_TEST_CASE(my_test6)
 BOOST_AUTO_TEST_CASE(my_test7)
 {
 	std::cout << "\n\n test7 Test array[0]\n";
-	struct AmuletCreateReq
-	{
-		INT64 arrayIdCostItem[0];  
-	};
 #ifdef _WIN32
 
-	AmuletCreateReq acr{ 0, 1, 2, 3, 4 };
+	AmuletCreateReq acr{ 1, 2, 3, 4, 5 };
 	for (int i = 0; i < 5; ++i)
 	{
 		std::cout<< acr.arrayIdCostItem[i]<<std::endl;
@@ -44,7 +53,7 @@ BOOST_AUTO_TEST_CASE(my_test7)
 	struct Tick 
 	{
 		AmuletCreateReq req;
-		int arr[5];
+		INT64 arr[5];
 	};
 
 	Tick tick{ {},{1,2,3,4,5} };
@@ -61,42 +70,22 @@ BOOST_AUTO_TEST_CASE(my_test7)
 BOOST_AUTO_TEST_CASE(my_test8)
 {
 	std::cout << "\n\n test8 Test array[0]\n";
-	struct AmuletCreateReq
-	{
-		int u32ItemType;  //合成物品类型
-		int	u8Num;
-		INT64 arrayIdCostItem[0];  
-	};
 	struct Tick
 	{
-		int u32ItemType;  //合成物品类型
-		int	u8Num;
 		INT64 arrayIdCostItem[5];
 	};
 
-	Tick tick{ 1,2,{1,2,3,4,5} };
+	Tick tick{ {1,2,3,4,5} };
 	AmuletCreateReq& arc = *  ( (AmuletCreateReq*)(&tick) );
-
-	for (int i = 0; i < 5; ++i)
-	{
-		std::cout << arc.arrayIdCostItem[i] << std::endl;
-	}
+	PrintArray0(arc);
 }
 
 BOOST_AUTO_TEST_CASE(my_test9)
 {
 	std::cout << "\n\n test9 Test array[0]\n";
-	struct AmuletCreateReq
-	{
-		int u32ItemType;  //合成物品类型
-		int	u8Num;
-		int arrayIdCostItem[0];  
-	};
 	struct Tick
 	{
-		int u32ItemType;  //合成物品类型
-		int	u8Num;
-		int arrayIdCostItem[5];
+		INT64 arrayIdCostItem[5];
 	};
 
 	union Req {
@@ -105,33 +94,23 @@ BOOST_AUTO_TEST_CASE(my_test9)
 	};
 
 	Req req;
-	req.tick = { 1,2, {1,2,3,4,5} };
+	req.tick = {{1,2,3,4,5} };
 	auto& arc = req.rec;
-
-	for (int i = 0; i < 5; ++i)
-	{
-		std::cout << arc.arrayIdCostItem[i] << std::endl;
-	}
+	req.rec.arrayIdCostItem[0] = 100000000;
+	PrintArray0(arc);
 }
 
 BOOST_AUTO_TEST_CASE(my_test10)
 {
 	std::cout << "\n\n test10 Test array[0]\n";
-	struct AmuletCreateReq
-	{
-		int arrayIdCostItem[0];  
-	};
 	auto pArc = (AmuletCreateReq*)malloc(sizeof(AmuletCreateReq) + sizeof(int) * 5);
 	memset(pArc, 0, sizeof(AmuletCreateReq) + sizeof(int) * 5);
 
 	for (int i = 0; i < 5; ++i)
 	{
-		pArc->arrayIdCostItem[i] = i+1;
+		pArc->arrayIdCostItem[i] = i + 1;
 	}
-	for (int i = 0; i < 5; ++i)
-	{
-		std::cout << pArc->arrayIdCostItem[i] << std::endl;
-	}
+	PrintArray0(*pArc);
 	free(pArc);
 }
 
