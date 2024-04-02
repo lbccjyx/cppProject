3412a5f634b04fc2c61fbaac87967295a96d6a81
lbccjyx <70558271+lbccjyx@users.noreply.github.com>
2024-04-03 00:34:36 +0800

    lambda的简单例子

--- CppSocketServer/test/unit_test5.cpp
+++ CppSocketServer/test/unit_test5.cpp
@@ -86,7 +86,6 @@ BOOST_AUTO_TEST_CASE(my_test23)
 	delete	c2;
 }
 
-// 测试用例24
 BOOST_AUTO_TEST_CASE(my_test24)
 {
 	std::cout << "\n\n test24 int超范围测试\n";
@@ -100,5 +99,41 @@ BOOST_AUTO_TEST_CASE(my_test24)
 	std::cout << "x = " << x << std::endl;
 	std::cout << "x1 = " << x1 << std::endl;
 
+}
+
+BOOST_AUTO_TEST_CASE(my_test25)
+{
+	std::cout << "\n\n test25 lambda\n";
+	int num = 100;
+	int num2 = 200;
+
+	// 无参就可以不要带()  结尾的()就是表示调用此lambda函数
+	[] {std::cout << "no params\n"; }();
+	[num] {std::cout << "one param by name:" << num << "\n"; }();
+	// 捕捉所有上下文成员变成值传递
+	[=] {std::cout << "all params pass by value: num1: " << num <<"\tnum2:"<<num2 << "\n"; }();
+	std::cout << "num&:" << &num << std::endl;
+	[&num] {std::cout << "pass one param by reference, num&:" << &num<<std::endl; }();
+	// 捕捉所有上下文成员变成引用传递
+	[&] {std::cout << "pass all params by reference, num&:" << &num << std::endl; }();
+	// 不捕捉 通过正常参数传递
+	[](int a, int b) {std::cout << "a+b:" << a + b << std::endl; }(1, 2);
+	// 只要是操作引用 即使没有mutable一样改
+	[&num] {std::cout << "add one by reference, num:" << ++num << std::endl; }();
+	std::cout << "num:" << num << std::endl;
+	// 即使是() 参数 如果加mutable 也需要加() 在这个例子中看不出mutable的作用
+	[&num]() mutable {std::cout << "add one by reference, num:" << ++num << std::endl; }();
+	std::cout << "num:" << num << std::endl;
+
+	std::vector<int> Arr{ 1,2,4,9,5,3 };
+	std::sort(Arr.begin(), Arr.end(), [=](int x, int y) {return x < y; });
+	int arr[6]{ 1,2,4,9,5,3 };
+	std::sort(arr, arr + 6, [=](int x, int y) {return x < y; });
+	for (auto i : arr)
+		std::cout << i << "\t";
+	std::cout<<std::endl;
+	for (auto i : Arr)
+		std::cout << i << "\t";
+	
 }
 BOOST_AUTO_TEST_SUITE_END()
