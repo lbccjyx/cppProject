78d8a0da70e73afef15cce98e2a2d0fff6fb7baa
lbccjyx <70558271+lbccjyx@users.noreply.github.com>
2024-06-28 02:22:32 +0800

    比较不正常但是宽度一致的连杆

--- CppSocketServer/src/ObjectThing/DraggableCircle2.h
+++ CppSocketServer/src/ObjectThing/DraggableCircle2.h
@@ -21,6 +21,35 @@ public:
 		velocity = sf::Vector2f(0, 0);
 	}
 
+	void setConnPosition(DraggableCircle* draggedCircle, DraggableCircle* otherCircle, const sf::Vector2f& position, const sf::Vector2f& offset)
+	{
+		// 更新被拖动的圆的位置
+		draggedCircle->setPosition(position, offset);
+
+		// 获取更新后两个圆的位置
+		sf::Vector2f draggedPosition = draggedCircle->getPosition() + sf::Vector2f(draggedCircle->getRadius(), draggedCircle->getRadius());
+		sf::Vector2f otherPosition = otherCircle->getPosition() + sf::Vector2f(otherCircle->getRadius(), otherCircle->getRadius());
+
+		// 计算连接轴的方向并确保其长度保持不变
+		sf::Vector2f direction = otherPosition - draggedPosition;
+		float currentLength = std::sqrt(direction.x * direction.x + direction.y * direction.y);
+
+		// 如果距离不等于连接轴的长度，则进行调整
+		if (currentLength != length)
+		{
+			direction /= currentLength;
+			otherPosition = draggedPosition + direction * length;
+			otherCircle->setPosition(otherPosition, sf::Vector2f(otherCircle->getRadius(), otherCircle->getRadius()));
+		}
+
+		// 更新连接轴的位置和旋转
+		sf::Vector2f newDraggedPosition = draggedCircle->getPosition() + sf::Vector2f(draggedCircle->getRadius(), draggedCircle->getRadius());
+		sf::Vector2f newOtherPosition = otherCircle->getPosition() + sf::Vector2f(otherCircle->getRadius(), otherCircle->getRadius());
+		connector.setPosition((newDraggedPosition + newOtherPosition) / 2.0f);
+		float angle = std::atan2(newOtherPosition.y - newDraggedPosition.y, newOtherPosition.x - newDraggedPosition.x);
+		connector.setRotation(angle * 180.0f / 3.14159265f);
+	};
+
 	void setPosition(const sf::Vector2f& position, const sf::Vector2f& offset)
 	{
 		if (this->CenterContains(position)) 
@@ -36,41 +65,12 @@ public:
 			// 判断具体是哪个圆被拖动，并设置指针
 			if (leftCircle.getGlobalBounds().contains(position))
 			{
-				draggedCircle = &leftCircle;
-				otherCircle = &rightCircle;
+				this->setConnPosition(&leftCircle, &rightCircle, position, offset);
 			}
 			else
 			{
-				draggedCircle = &rightCircle;
-				otherCircle = &leftCircle;
+				this->setConnPosition(&rightCircle, &leftCircle, position, offset);
 			}
-
-			// 更新被拖动的圆的位置
-			draggedCircle->setPosition(position , -offset);
-
-			// 获取更新后两个圆的位置
-			sf::Vector2f draggedPosition = draggedCircle->getPosition() + sf::Vector2f(draggedCircle->getRadius(), draggedCircle->getRadius());
-			sf::Vector2f otherPosition = otherCircle->getPosition() + sf::Vector2f(otherCircle->getRadius(), otherCircle->getRadius());
-
-			// 计算连接轴的方向并确保其长度保持不变
-			sf::Vector2f direction = otherPosition - draggedPosition;
-			float currentLength = std::sqrt(direction.x * direction.x + direction.y * direction.y);
-
-			// 如果距离不等于连接轴的长度，则进行调整
-			if (currentLength != length)
-			{
-				direction /= currentLength;
-				otherPosition = draggedPosition + direction * length;
-				otherCircle->setPosition(otherPosition ,-sf::Vector2f(otherCircle->getRadius(), otherCircle->getRadius()));
-			}
-
-			// 更新连接轴的位置和旋转
-			sf::Vector2f newDraggedPosition = draggedCircle->getPosition() + sf::Vector2f(draggedCircle->getRadius(), draggedCircle->getRadius());
-			sf::Vector2f newOtherPosition = otherCircle->getPosition() + sf::Vector2f(otherCircle->getRadius(), otherCircle->getRadius());
-			connector.setPosition((newDraggedPosition + newOtherPosition) / 2.0f);
-			float angle = std::atan2(newOtherPosition.y - newDraggedPosition.y, newOtherPosition.x - newDraggedPosition.x);
-			connector.setRotation(angle * 180.0f / 3.14159265f);
-		
 		};
 	}
 
@@ -147,45 +147,22 @@ public:
 			}
 
 		}
-		
-		// Update positions of the circles
-		leftCircle.setPosition(leftCircle.getPosition(),leftVelocity * deltaTime);
-		rightCircle.setPosition(rightCircle.getPosition(),rightVelocity * deltaTime);
-
 		// Prevent circles from going below ground
-		if (leftCircle.getPosition().y > GROUND_HEIGHT)
+		if (leftCircle.getPosition().y + leftVelocity.y * deltaTime > GROUND_HEIGHT)
 		{
-			leftCircle.setPosition(sf::Vector2f(leftCircle.getPosition().x, GROUND_HEIGHT), sf::Vector2f(0,0));
+			this->setConnPosition(&leftCircle, &rightCircle, sf::Vector2f(leftCircle.getPosition().x, GROUND_HEIGHT), sf::Vector2f(0, 0));
 			leftVelocity.y = 0;
 		}
 
-		if (rightCircle.getPosition().y > GROUND_HEIGHT)
+		if (rightCircle.getPosition().y + leftVelocity.y * deltaTime > GROUND_HEIGHT)
 		{
-			rightCircle.setPosition(sf::Vector2f(rightCircle.getPosition().x, GROUND_HEIGHT), sf::Vector2f(0, 0));
+			this->setConnPosition(&rightCircle, &leftCircle, sf::Vector2f(rightCircle.getPosition().x, GROUND_HEIGHT), sf::Vector2f(0, 0));
 			rightVelocity.y = 0;
 		}
+		// Update positions of the circles
+		this->setConnPosition(&rightCircle, &leftCircle, leftCircle.getPosition(), leftVelocity * deltaTime);
+		this->setConnPosition(&leftCircle, &rightCircle, rightCircle.getPosition(), rightVelocity * deltaTime);
 
-		// Update connector position
-		sf::Vector2f leftPosition = leftCircle.getPosition();
-		sf::Vector2f rightPosition = rightCircle.getPosition();
-
-		// Ensure fixed length
-		sf::Vector2f direction = rightPosition - leftPosition;
-		float currentLength = std::sqrt(direction.x * direction.x + direction.y * direction.y);
-		direction /= currentLength;
-
-		if (currentLength > length)
-		{
-			rightCircle.setPosition(leftPosition, direction * length);
-			rightPosition = leftPosition + direction * length;
-		}
-
-		// Update the position of the connector to be between the two circles
-		connector.setPosition((leftPosition + rightPosition) / 2.0f);
-
-		// Calculate the angle of the connector based on the positions of the circles
-		float angle = std::atan2(rightPosition.y - leftPosition.y, rightPosition.x - leftPosition.x);
-		connector.setRotation(angle * 180.0f / 3.14159265f);
 	}
 
 
